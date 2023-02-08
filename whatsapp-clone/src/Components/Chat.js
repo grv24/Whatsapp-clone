4ddb0fe196f18aa32838cbb1f6be1d77094ff493
grv24 <108221462+grv24@users.noreply.github.com>
2023-02-09 02:11:35 +0530

    new

--- whatsapp-clone/src/Components/Chat.js
+++ whatsapp-clone/src/Components/Chat.js
@@ -1,16 +1,38 @@
 import { AttachFile, InsertEmoticon, MoreVert, SearchOutlined } from '@mui/icons-material'
 import { Avatar, IconButton } from '@mui/material'
 import MicIcon from '@mui/icons-material/Mic';
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import'./Chat.css'
+import { useParams } from 'react-router-dom';
+import db from './Config'
 
 function ChatBar() {
+
+    const [input, setInput] = useState("");
+    const {roomId} = useParams();
+    const [roomName,setRoomName] = useState("");
+    
+    useEffect(()=>{
+       if(roomId){
+        db.collection("rooms")
+        .doc(roomId)
+        .onSnapshot((snapshot)=>setRoomName
+        (snapshot.data().name));
+       }
+    },[roomId])
+    
+
+     const sendMessage=(e)=>{
+        e.preventDefault();
+        console.log(input)
+        setInput("")
+      }
   return (
     <div className='chat'>
          <div className='chat__header'>
-             <Avatar/>
-             <div className='chat__headerInfo'>
-                <h3>Room name</h3>
+         <Avatar src={`https://avatars.dicebear.com/api/human/${Math.floor(Math.random() * 5000)}.svg`} />
+         <div className='chat__headerInfo'>
+                <h3>{roomName}</h3>
                 <p>Last Seen at....</p>
              </div>
              <div className='chat__headerRight'>
@@ -26,38 +48,26 @@ function ChatBar() {
              </div>
          </div>
          <div className='chat__body'>
-              <p className='chat__message'>
-              <span className='chat__name'>Sonnny</span>
+              <p className={`chat__message ${true &&"chat__receiver"}`}>
+              <span className='chat__name'>Sonny</span>
                   This is a message
               <span className='chat__timestamp'>
                   {new Date().toUTCString()}
               </span>
             </p>
-            <p className='chat__message chat__receiver'>
-            <span className='chat__name'>Sonnny</span>
-                This is a message
-            <span className='chat__timestamp'>
-                {new Date().toUTCString()}
-            </span>
-            </p>
-            <p className='chat__message'>
-            <span className='chat__name'>Sonnny</span>
-                This is a message
-            <span className='chat__timestamp'>
-                {new Date().toUTCString()}
-            </span>
-          </p>
-          
          </div>
           
       <div className='chat__footer'>
          <InsertEmoticon/>
          <form>
             <input
+                value={input}
+                onChange={e=>setInput(e.target.value)}
                 placeholder='Type a message'
                 type='Text'
               />
               <button
+                 onClick={sendMessage}
                  type='submit'>
                  Send a message
               </button>
